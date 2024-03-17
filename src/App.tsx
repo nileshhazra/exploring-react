e4bcd7ae89dc5c7534eab45fec7f1f8e243bd6bd
nileshhazra <nileshhz99@gmail.com>
2024-03-18 01:02:45 +0530

    add ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from './components/ListGroup';
 function App() {
+  const items = ['London', 'New York', 'Tokyo', 'San Francisco', 'Paris'];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} />
     </div>
   );
 }
